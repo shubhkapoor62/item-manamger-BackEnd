dab879546fcb41f393d33c9fa6696bc17d0e3496
unknown <shubhkapoor62@gmail.com>
2021-04-17 18:13:15 -0700

    ADD permission Level

--- users/user.model.js
+++ users/user.model.js
@@ -6,7 +6,8 @@ const schema = new Schema({
     hash: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    createdDate: { type: Date, default: Date.now }
+    createdDate: { type: Date, default: Date.now },
+    permissionLevel: {type: Number, default: 2, required: true}
 });
 
 schema.set('toJSON', {
